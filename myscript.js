f76a1f779a98416717eadbe385bc29993174efed
haffiiorn <haffiiorn@gmail.com>
2014-12-05 17:53:23 +0000

    YEah boi

--- myscript.js
+++ myscript.js
@@ -1,32 +1,66 @@
 var database = [];
-$(document).ready(function() {
-	var breyta;
-	for(var i = 1950; i<2015; i++)
-	{
-		dostuff("data/"+i+".json");
-	}
-	console.log(database);
-});
+google.load('visualization', '1.0', {'packages':['corechart']});
+google.setOnLoadCallback(start);
 
-function dostuff(breyta){
-$.getJSON(breyta, function(data) {
-	var arr=[];
-	var last;
-	for(var i = 0; i <data.length;i++)
-	{
-		if(!(data[i].Player == last)){
-		 	if(!(data[i].Player=="Player")){
-				last = data[i].Player;
-				arr.push(data[i]);
+function start(){
+	
+	 	var breyta;
+		for(var i = 1950; i<2015; i++)
+		{
+		dostuff(i);	
+	}
+	}
+
+	function dostuff(breyta){
+	$.getJSON("data/"+breyta+".json", function(data) {
+		var arr=[];
+		var last;
+		for(var i = 0; i <data.length;i++)
+		{
+			if(!(data[i].Player == last)){
+			 	if(!(data[i].Player=="Player")){
+					last = data[i].Player;
+					arr.push(data[i]);
+				}
 			}
-		}
+			
+		}	
 		
-	}	
-	var avg = 0;
-for(var i = 0; i<arr.length;i++)
-{
-	avg += parseInt(arr[i].PTS);
-}
-database.push(arr);
-});
-}
+	database.push(arr);
+	if(breyta==2014)
+	{
+		drawChart();	
+	}
+	});
+	}
+
+		
+      function drawChart() {
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Year');
+        data.addColumn('number', 'Points');
+        console.log(database.length)
+     var tots = 0;
+	for(var i = 0; i<database.length;i++)
+	{
+		for(var x = 0;x<database[i].length;x++)
+		{
+			tots += parseInt(database[i][x].PTS);
+
+		}
+        data.addRows([
+          [String(1950+i), tots]
+        ]);
+        tots=0;
+	}
+        // Set chart options
+        var options = {'title':'Points Scored',
+                       'width':800,
+                       'height':500};
+
+        var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
+
+	
+
